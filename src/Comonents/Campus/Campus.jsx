24fda536bafe449270912c3d1230274e757830e7
otiendeneville <otiendeneville@gmail.com>
2025-11-11 00:34:55 +0300

    animation and onClick functionality

--- src/Comonents/Campus/Campus.jsx
+++ src/Comonents/Campus/Campus.jsx
@@ -4,16 +4,15 @@ import gallery1 from "../../assets/gallery1.jpg";
 import gallery2 from "../../assets/gallery2.jpg";
 import gallery3 from "../../assets/gallery3.jpg";
 import gallery4 from "../../assets/gallery4.jpg";
+// import { useState } from "react";
 
 const Campus = () => {
-  const [state, setState] = "useState";
+  const [message, setMessage] = useState("");
   const handleClick = () => {
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-      veritatis, tempora nobis quas tenetur voluptatibus architecto consequuntur
-      quidem vitae nisi qui debitis amet voluptatem officia totam et expedita
-      fuga quae?
-    </p>;
+    <p> the message is{message} </p>;
+    setMessage(
+      "What you the world like butterflies so the world right or wrong like  keeping getting lost in the sky are you letting you down ain't letting you down brcarry you around ain't letting you down,why though we stop and i know you scared name a time you don't have to be afraid "
+    );
   };
   return (
     <div id="campus" className="campus">
@@ -36,6 +35,7 @@ const Campus = () => {
           <path d="m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z" />
         </svg>
       </button>
+      {message && <p>{message}</p>}
     </div>
   );
 };
